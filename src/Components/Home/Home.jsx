d9d0941dc977afa0b1739edfeeb5003eec48ee8d
unknown <muzammilsharif096@gmail.com>
2024-05-16 15:07:31 -0700

    Add

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import data from '../../Data'
 export default function Home() {
+  const lenevo = data.filter((ele) => ele.brand === "LENEVO")
   return (
-    <div>
-      Home
-    </div>
+    <div className='bg-neutral-100'>
+      <div className='flex justify-between px-16'>
+        <p className='font-medium text-lg text-blue-500'>Lenovo Audio Equipment</p>
+        <p className='text-cyan-500 text-md'>View all lenovo earbuds </p>
+      </div>
+      <div className='grid grid-cols-6 px-16 '>
+        {
+          lenevo.map((ele) => {
+            return (
+              <div className='border bg-white'>
+                <div className='flex w-52 justify-center'>
+                  <img src={ele.img} alt="" />
+                </div>
+                <h1>{ele.brand}</h1>
+                <p>{ele.title}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+
+    </div >
   )
 }
